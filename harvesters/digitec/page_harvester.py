c91e74a16ad3e80b5bf1168924a099113d8e67d3
Dennis Ligtenberg <dennis_ligtenberg@hotmail.com>
2016-12-03 11:47:25 +0100

    Added special case for christmas

--- harvesters/digitec/page_harvester.py
+++ harvesters/digitec/page_harvester.py
@@ -1,11 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
+import datetime
+
+
+def request_page(url_extension):
+    headers = {"user-agent": "Mozilla/5.0"}
+    r = requests.get("https://www.digitec.ch" + url_extension, headers=headers).text
+    return BeautifulSoup(r, "lxml")
+
+
+def get_product_url_advent_calendar():
+    soup = request_page("/de/LiveShopping/AdventCalendar")
+    overlay_elements = soup.find(id="03").find_all("a", class_="overlay")
+    return overlay_elements[0].get("href")
+
+
+def get_product_url():
+    soup = request_page("/de/LiveShopping")
+    overlay_elements = soup.find_all("a", class_="overlay")
+    return overlay_elements[0].get("href")
 
 
 def harvest_page():
-    headers = {'user-agent': 'Mozilla/5.0'}
-    r = requests.get('https://www.digitec.ch/de/LiveShopping', headers=headers).text
-    soup = BeautifulSoup(r, 'lxml')
-    overlay_elements = soup.find_all("a", class_="overlay")
-    deal_path = overlay_elements[0].get('href')
-    print(deal_path)
+    current_date = datetime.datetime.now()
+    if current_date.month == 12 and current_date.day < 25:
+        product_url = get_product_url_advent_calendar()
+    else:
+        product_url = get_product_url()
+    print(product_url)
